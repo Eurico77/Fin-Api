6db712989971a5de5eeec02168daf80c5b080cc7
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-03-14 19:10:49 -0300

    add rota de saque

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const { v4: uuid } = require('uuid');
-const { customers } = require('../util')
+const { customers, getBalance } = require('../util')
 const verifyIfExistsAccount = require('../middlewares/verifyExistsAccount')
 
 const routes = Router()
@@ -32,9 +32,46 @@ routes.get('/account', (req, res) => {
   return res.json(customers)
 })
 
-routes.get('/statements', verifyIfExistsAccount, (req, res, next) => {
+routes.get('/statements', verifyIfExistsAccount, (req, res) => {
   const { customer } = req
   return res.json(customer.statement)
 })
 
-module.exports = routes, customers 
+routes.post('/deposit', verifyIfExistsAccount, (req, res) => {
+  const { amount, description } = req.body
+  const { customer } = req;
+
+  const statementOperation = {
+    amount,
+    description,
+    created_at: new Date(),
+    type: 'credit'
+  }
+  customer.statement.push(statementOperation)
+
+  return res.status(201).send()
+
+})
+
+routes.post('/withdraw', verifyIfExistsAccount, (req, res) => {
+  const { amount } = req.body
+  const { customer } = req
+
+  const balance = getBalance(customer)
+
+  if(balance < amount) {
+   return res.status(400).json({error: 'Insuficient found!'})
+  }
+
+  const statementOperation = {
+    amount,
+    updated_at: new Date(),
+    type: 'debit'
+
+  }
+  customer.statement.push(statementOperation)
+  return res.status(201).send()
+
+})
+
+module.exports = routes 
