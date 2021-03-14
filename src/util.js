4e7dfa1b936b2e925e17d922d89a688ef7844a68
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-03-14 19:09:51 -0300

    add util de verificar balanco

--- src/util.js
+++ src/util.js
@@ -1,3 +1,15 @@
 module.exports = {
-    customers: []
+    customers: [],
+    getBalance: ({ statement }) => {
+        const result = statement.reduce((acc, index) => {
+            if(index.type === 'credit') {
+                return  acc + index.amount 
+
+            }else {
+              return acc - index.amount  
+            }  
+        }, 0)
+
+        return result;
+    }
 }
