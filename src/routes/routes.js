5459a207986ec503514d0a03f1b72bbf4c2f7727
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-03-14 14:04:25 -0300

    refatorando

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express')
+const { Router, response } = require('express')
 const { v4: uuid } = require('uuid');
 
 const routes = Router()
@@ -7,11 +7,13 @@ const costumers = [];
 routes.post('/account', (req, res) => {
    const { cpf, name } = req.body
 
-   const cpfAlreadExists = costumers.find(costumer => costumer.cpf === cpf)
+   const cpfAlreadExists = costumers.some(
+     costumer => costumer.cpf === cpf
+    )
 
     if(cpfAlreadExists){
-     return  res.status(400).json({ message: 'cpf já cadastrado'})
-
+     return  res.status(400)
+     .json({ message: 'cpf já cadastrado'})
    }
 
    costumers.push({
@@ -23,13 +25,16 @@ routes.post('/account', (req, res) => {
 
    return res.status(201).send()
 
-
 })
 
 routes.get('/statements/:cpf', (req, res) => {
     const { cpf } = req.params
 
-    
+    const costumer = costumers.filter(
+      costumer => costumer.cpf === cpf
+    )
+
+    return res.json(costumer.statement)
 })
 
 module.exports = routes
