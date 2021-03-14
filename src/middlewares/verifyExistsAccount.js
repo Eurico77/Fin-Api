5f379282f556dd011d51fc0e73c99fb486468af8
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-03-14 19:10:25 -0300

    add validacao de conta inexistente

--- src/middlewares/verifyExistsAccount.js
+++ src/middlewares/verifyExistsAccount.js
@@ -1,17 +1,24 @@
 const { customers } = require('../util')
 
 function verifyIfExistsAccount(req, res, next) {
-  const { cpf } = req.headers;
+  try { 
 
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return res.status(400).json({ error: 'user not found' });
+    const { cpf } = req.headers;
+  
+    const customer = customers.find((customer) => customer.cpf === cpf);
+  
+    if (!customer) {
+      return res.status(400).json({ error: 'user not found' });
+    }
+  
+    req.customer = customer;
+  
+    return next()
+    
+  } catch (error) {
+    throw new Error(error)
   }
-
-  req.customer = customer;
-
-  return next()
+  
   
 };
 
